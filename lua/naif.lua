38d9e9fae003d36f69df792cafc94782515eb509
mvllow <mvllow@icloud.com>
2023-05-14 00:02:55 +0200

    init

---
+++ lua/naif.lua
@@ -0,0 +1,104 @@
+local M = {}
+M.options = {}
+
+local function get_highlight(group)
+	local highlight = vim.api.nvim_get_hl(0, { name = group })
+	return { bg = highlight.bg or "", fg = highlight.fg or "" }
+end
+
+local function set_highlight(group, color)
+	vim.api.nvim_set_hl(0, group, color)
+end
+
+local function get_palette(overrides)
+	overrides = overrides or {}
+	local palette = {
+		strong = { fg = get_highlight("Normal").fg, bold = true },
+		normal = { fg = get_highlight("Normal").fg, bg = get_highlight("Normal").bg },
+		critical = { fg = get_highlight("Normal").fg, bg = get_highlight("ErrorMsg").fg },
+		popout = { fg = get_highlight("Number").fg },
+		salient = { fg = get_highlight("Type").fg },
+		faded = { fg = get_highlight("Comment").fg },
+		subtle = { fg = get_highlight("Comment").fg, bg = get_highlight("StatusLine").bg },
+	}
+	return vim.tbl_deep_extend("force", palette, overrides)
+end
+
+function M.disable_semantic_highlights()
+	for _, group in ipairs(vim.fn.getcompletion("@", "highlight")) do
+		vim.api.nvim_set_hl(0, group, {})
+	end
+end
+
+function M.disable_treesitter_highlights()
+	vim.cmd("TSDisable highlight")
+end
+
+function M.setup(options)
+	options = options or {}
+	options.palette = options.palette or {}
+
+	M.options = vim.tbl_deep_extend("force", { disable_extra_highlights = true, palette = get_palette() }, options)
+
+	M.apply(get_palette(options.palette))
+
+	-- Set highlights when colorscheme changes
+	vim.api.nvim_create_autocmd("ColorScheme", {
+		pattern = "*",
+		callback = function()
+			M.apply(get_palette(options.palette))
+		end,
+	})
+
+	if M.options.disable_extra_highlights then
+		vim.api.nvim_create_autocmd({ "ColorScheme", "LspAttach" }, {
+			callback = M.disable_semantic_highlights,
+		})
+
+		vim.api.nvim_create_autocmd("BufEnter", {
+			callback = M.disable_treesitter_highlights,
+		})
+	end
+end
+
+function M.apply(palette)
+	M.options.palette = palette
+
+	-- Strong
+	set_highlight("Function", palette.strong)
+
+	-- Normal
+	set_highlight("Conditional", palette.normal)
+	set_highlight("Constant", palette.normal)
+	set_highlight("Exception", palette.normal)
+	set_highlight("Identifier", palette.normal)
+	set_highlight("Include", palette.normal)
+	set_highlight("Keyword", palette.normal)
+	set_highlight("Label", palette.normal)
+	set_highlight("Normal", palette.normal)
+	set_highlight("Operator", palette.normal)
+	set_highlight("PreProc", palette.normal)
+	set_highlight("Repeat", palette.normal)
+	set_highlight("Special", palette.normal)
+	set_highlight("Statement", palette.normal)
+	set_highlight("Structure", palette.normal)
+
+	-- Critical
+	set_highlight("ErrorMsg", palette.critical)
+
+	-- Popout
+
+	-- Salient
+	set_highlight("Boolean", palette.salient)
+	set_highlight("Constant", palette.salient)
+	set_highlight("Number", palette.salient)
+	set_highlight("String", palette.salient)
+
+	-- Faded
+	set_highlight("Comment", palette.faded)
+
+	-- Subtle
+	set_highlight("StatusLine", palette.subtle)
+end
+
+return M
---
+++ readme.md
@@ -0,0 +1,47 @@
+# naïf.nvim
+
+> Naïve syntax highlighting.
+
+**This plugin is experimental and a work-in-progress. Neovim nightly is recommended, as a currently unreleased [pull request](https://github.com/neovim/neovim/pull/22693) is utilised.**
+
+## Introduction
+
+_[On the design of text editors](https://arxiv.org/pdf/2008.06030.pdf)_ by Nicolas P. Rougier–as the title suggests–discusses the design of text editors, including the lack of evidence supporting the utility of syntax highlighting for code comprehension. Naïf reduces your current theme's palette while applying opinionated design principals regarding typography and colour. Try naïf with your favourite theme and enjoy the labours of a well-crafted interface and elegant syntax highlighting.
+
+Naïf tries not to interfere with plugins, diagnostics, or other non-syntax related highlighting.
+
+## Usage
+
+Install naïf via your favourite plugin manager, then initialise the plugin.
+
+```lua
+require("naif").setup()
+```
+
+## Options
+
+Naïf is intended to work without any extra configuration. Options should be considered a method for fixing undesirable or broken defaults.
+
+**disable_extra_highlights**
+
+Naïf manages basic syntax groups only, disabling treesitter highlights and semantic tokens by default. Changing this option from the default value will introduce a more colourful–less naïf–syntax.
+
+```lua
+disable_extra_highlights = true
+```
+
+**palette**
+
+Please note that the API for overriding palette values may change and is still considered experimental. Try naïf with your existing theme before overriding colours and please create an issue if something seems off.
+
+```lua
+palette = {
+    strong = { fg = "#2d2d2d", bold = true },
+    normal = { fg = "#2d2d2d", bg = "#ffffff" },
+    critical = { fg = "#ffffff", bg = "#ff0000" },
+    popout = { fg = "#ff9301" },
+    salient = { fg = "#5856d7" },
+    faded = { fg = "#999999" },
+    subtle = { fg = "#2d2d2d", bg = "#f0f0f0" },
+}
+```
